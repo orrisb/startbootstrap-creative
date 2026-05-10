0a97cfff1724d3ef07db3b9c019d7e26e024d262
orris baker <internetearnings@live.com>
2026-05-10 04:50:45 -0400

    Refactor jQuery scripts for toggle and hover effects

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -5,7 +5,33 @@
 */
 //
 // Scripts
-// 
+// 01 document ready 
+$(function(){
+  //alert('Welcome to StarTrackr! Now no longer under police investigation!');
+  //alert($('#celebs tbody tr:even').length + ' elements!');
+ //$('#celebs tbody  tr:even').addClass('zebra');
+  
+  $('<input type="button" value="toggle"  id="toggleButton">').insertBefore('#disclaimer');
+  $('#toggleButton').click(function() {
+    $('#disclaimer').slideToggle('slow',function() {
+      $('#toggleButton').fadeOut();
+    });
+  });
+  
+  $('<strong>START!</strong>').prependTo('#disclaimer');
+  $('<strong>END!</strong>').appendTo('#disclaimer');*/
+  
+  $('#no-script').remove();
+  
+  $('#celebs tr').mouseover(function() {
+    $(this).addClass('zebraHover');
+  });
+  
+  $('#celebs tr').mouseover(function() {
+    $(this).removeClass('zebraHover');
+  });
+
+});
 
 window.addEventListener('DOMContentLoaded', event => {
 
